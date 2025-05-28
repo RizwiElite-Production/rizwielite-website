d5d1600221265edb6cff94a9ad64afe1f03942c8
RizwiElite-Production <rizwikhan56765@gmail.com>
2025-05-28 12:52:30 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-// You can add interactive features later here
-console.log("Website Loaded");
+// Mobile Menu Toggle
+document.querySelector('.hamburger')?.addEventListener('click', () => {
+  document.querySelector('.mobile-nav').classList.toggle('active');
+});
